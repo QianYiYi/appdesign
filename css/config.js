837bb123f452263a366bd89223f81517ec5c9713
QianYiYi <2441168440@qq.com>
2021-05-25 15:42:02 +0800

    first commit

---
+++ css/config.js
@@ -0,0 +1,25 @@
+const windowPort = "http://" + window.location.host + '/';
+
+// const timeBlockUrl = windowPort + "blocks/getblocksbytime/";
+const heightBlockUrl = windowPort + "blocks/getblockbyheight/";
+const hashBlockUrl = windowPort + "blocks/getblockbyhash/";
+
+// const timeTransactionUrl = windowPort + "tx/gettxbytime/";
+const idTransactionUrl = windowPort + "tx/gettxbyid/";
+const hashTransactionUrl = windowPort + "tx/gettxbyblock/";
+
+// const contractUrl = "assets/json/network.json";
+// const networkUrl = windowPort + "nodes/getinfos";
+
+
+// const loginUrl = windowPort + 'loginIn';
+
+// 测试
+const timeBlockUrl = "assets/json/blockNumber.json";
+
+const timeTransactionUrl = "assets/json/transaction.json";
+
+const loginUrl = "assets/json/login.json";
+
+const networkUrl = "assets/json/network.json";
+const contractUrl = "assets/json/network.json";
